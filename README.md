2b5e518c07692b75202c8236fe9e46bdd9ae5d95
Oleh Horitsyn <oleg.goritsyn@gmail.com>
2021-09-17 12:42:32 +0300

    add simple TCP chat

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # chatico
 Chat server written with Rust and Tokio
+
+### Run
+Navigate to directory and run `cargo run`
+
+Then you are able to connect using a `telnet localhost 8080`.
+Connect a few clients and type something.
+
+### TIL
+`#[tokio::main]` is used to make async functions work w/o adding a boilerplate.
+
+There are different ways to write async code.
+`spawn` spawns a task which is executed concurrently
+```rust
+tokio::spawn(async move {
+// async code
+});
+```
+
+In case you need to execute tasks which are independent and just use some shared state
+you can use a macro `tokio::select!`
+```rust
+tokio::select! {
+    result = some_async_fn() => {
+        let data = result.unwrap();
+        // use result if needed
+    }
+    _ = other_async_fn() => {
+        // or just execute a callback
+    }
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, _rx) = broadcast::channel(10);
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (read_half, mut write_half) = socket.split();
+
+            let mut reader = BufReader::new(read_half);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, sender_addr) = result.unwrap();
+                        if addr != sender_addr {
+                            write_half.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
